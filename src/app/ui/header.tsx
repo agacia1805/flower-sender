065b00dec99bdfab20536dfe3a6ff330aff3a7b8
agacia1805 <agacia1805@gmail.com>
2024-02-09 20:14:31 +0000

    (chore) update header component

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import Logo from './logo';
 
 export default function Header() {
   return (
     <header className='flex justify-center p-10 md:justify-start'>
       <Link href='/' passHref>
+        <Logo />
         <span className='sr-only'>Flower sender app homepage</span>
       </Link>
     </header>
