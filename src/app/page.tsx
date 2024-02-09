5c442b98e30adf1ee6306f163953c332f1ccf385
agacia1805 <agacia1805@gmail.com>
2024-02-09 20:16:52 +0000

    (feat) add base components and logic to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
-import Image from 'next/image';
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import Button from '@/app/ui/button';
+import Flower from '@/app/ui/flower';
+import { flowerGroups } from '@/app/ui/flowerGroups';
 
 export default function Home() {
+  const [selectedGroup, setSelectedGroup] = useState(null);
+
+  const handleButtonClick = () => {
+    const randomIndex = Math.floor(Math.random() * flowerGroups.length);
+    const randomGroup = flowerGroups[randomIndex];
+    setSelectedGroup(randomGroup);
+  };
+
   return (
-    <main></main>
+    <div className='fixed left-0 flex w-full items-center justify-center'>
+      {selectedGroup &&
+        selectedGroup.subgroups.map((subgroup, index) =>
+          subgroup.flowers.map((flower) => (
+            <Flower
+              key={`${flower.id}-${index}`}
+              flower={flower}
+              animate={true}
+            />
+          ))
+        )}
+      <Button onClick={handleButtonClick} name='Show flowers button'>
+        Open me
+      </Button>
+    </div>
   );
 }
