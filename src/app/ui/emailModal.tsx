6c85dceb06889763cc8295fa373999919b176b83
agacia1805 <agacia1805@gmail.com>
2024-02-12 21:02:56 +0000

    (chore) update modal component imports

--- src/app/ui/emailModal.tsx
+++ src/app/ui/emailModal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Fragment, useRef, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react'
 
 interface DialogProps {
   onClose: () => void;
@@ -58,9 +59,6 @@ export default function EmailModal({ isOpen, onClose }: DialogProps) {
                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                      <div className="sm:flex sm:items-start">
-                       <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                         <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-                       </div>
                        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                          <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
                            Deactivate account
