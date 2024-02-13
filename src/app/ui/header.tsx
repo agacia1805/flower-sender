6736d448d87f02cbdd754ec5220262a7125d10f2
agacia1805 <agacia1805@gmail.com>
2024-02-13 20:58:50 +0000

    (feat) add share by email button to header

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link';
 import Logo from './logo';
+import ShareEmail from './shareEmail';
 
 export default function Header() {
   return (
-    <header className='flex justify-center p-10 md:justify-start'>
+    <header className='flex justify-between px-6 py-10 md:p-10'>
       <Link href='/' passHref>
         <Logo />
         <span className='sr-only'>Flower sender app homepage</span>
       </Link>
+      <ShareEmail />
     </header>
   );
 }
