16d5157041bafdf02912f2d881ff0606e00a843d
agacia1805 <agacia1805@gmail.com>
2024-02-13 20:57:44 +0000

    (feat) improve share flower by email modal and add success and error messages

--- src/app/ui/emailModal.tsx
+++ src/app/ui/emailModal.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { Fragment, useRef, useState } from 'react';
-import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState, useCallback } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import Button from './button';
 
 interface DialogProps {
   onClose: () => void;
@@ -11,6 +12,7 @@ interface DialogProps {
 export default function EmailModal({ isOpen, onClose }: DialogProps) {
   const [email, setEmail] = useState<string>('');
   const [senderName, setSenderName] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -20,82 +22,129 @@ export default function EmailModal({ isOpen, onClose }: DialogProps) {
       body: JSON.stringify({ email, senderName }),
     });
     if (response.ok) {
-      alert('Email sent successfully!');
-      onClose();
+      setMessage('Flowers were sent successfully. Thank you for sharing love!');
     } else {
-      alert('Failed to send email. Test');
-      console.log(response);
+      setMessage('Something went wrong. Please try again later.');
     }
   };
+
+  const handleCloseDialog = useCallback(() => {
+    onClose();
+    setMessage('');
+    setEmail('');
+    setSenderName('');
+  }, []);
 
   if (!isOpen) return null;
 
   return (
     <Transition.Root show={isOpen} as={Fragment}>
-         <Dialog as="div" className="relative z-10" onClose={onClose}>
-           <Transition.Child
-             as={Fragment}
-             enter="ease-out duration-300"
-             enterFrom="opacity-0"
-             enterTo="opacity-100"
-             leave="ease-in duration-200"
-             leaveFrom="opacity-100"
-             leaveTo="opacity-0"
-           >
-             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-           </Transition.Child>
-
-           <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-             <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-               <Transition.Child
-                 as={Fragment}
-                 enter="ease-out duration-300"
-                 enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                 enterTo="opacity-100 translate-y-0 sm:scale-100"
-                 leave="ease-in duration-200"
-                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-               >
-                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                   <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                     <div className="sm:flex sm:items-start">
-                       <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                         <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                           Send flowers to your loved ones
-                         </Dialog.Title>
-                         <div className="mt-2">
-                           <p className="text-sm text-gray-500">
-                             Share the love.
-                             Add email address(es) and your name (or stay anonymous).
-                           </p>
-                         </div>
-                       </div>
-                     </div>
-                   </div>
-                   <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                     <form onSubmit={handleSubmit}>
-                       <input
-                         type='email'
-                         placeholder="Enter recipient's email"
-                         value={email}
-                         onChange={(e) => setEmail(e.target.value)}
-                         required
-                       />
-                       <input
-                         type='text'
-                         placeholder='Enter your name'
-                         value={senderName}
-                         onChange={(e) => setSenderName(e.target.value)}
-                         required
-                       />
-                       <button type='submit'>Send Flowers</button>
-                     </form>
-                   </div>
-                 </Dialog.Panel>
-               </Transition.Child>
-             </div>
-           </div>
-         </Dialog>
-       </Transition.Root>
+      <Dialog as='div' className='relative z-10' onClose={handleCloseDialog}>
+        <Transition.Child
+          as={Fragment}
+          enter='ease-out duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='fixed inset-0 bg-gray-600 bg-opacity-75 transition-opacity' />
+        </Transition.Child>
+        <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
+          <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+              enterTo='opacity-100 translate-y-0 sm:scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+            >
+              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-gray-200 text-left text-purple-900 shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+                {message ? (
+                  <div className='flex flex-col gap-6 p-6 md:p-8'>
+                    <Dialog.Title
+                      as='h2'
+                      className='text-center text-lg font-semibold leading-6 text-purple-900'
+                    >
+                      {message}
+                    </Dialog.Title>
+                    <div className='flex gap-4'>
+                      <Button
+                        name='Send more flowers button'
+                        className='self-center bg-purple-900 text-sm text-white md:w-full'
+                        onClick={() => {
+                          setMessage('');
+                          setEmail('');
+                          setSenderName('');
+                        }}
+                      >
+                        Send more
+                      </Button>
+                      <Button
+                        name='Close dialog button'
+                        className='self-center bg-gray-400 text-sm text-white md:w-full'
+                        onClick={handleCloseDialog}
+                      >
+                        Cancel
+                      </Button>
+                    </div>
+                  </div>
+                ) : (
+                  <>
+                    <div className='p-6 md:p-8'>
+                      <div className='flex flex-col items-center'>
+                        <Dialog.Title
+                          as='h2'
+                          className='text-lg font-semibold leading-6 text-purple-900'
+                        >
+                          Share the love
+                        </Dialog.Title>
+                        <div className='mt-2'>
+                          <p className='text-sm text-gray-500'>
+                            Add email address and your name (or stay anonymous).
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='px-6 pb-6 pt-0 md:px-8 md:pb-8'>
+                      <form
+                        onSubmit={handleSubmit}
+                        className='flex flex-col items-center gap-2'
+                      >
+                        <input
+                          type='text'
+                          placeholder='Name'
+                          value={senderName}
+                          className='block w-full rounded-md border-0 px-3 py-1.5 text-sm text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-900 sm:leading-6 md:w-10/12'
+                          onChange={(e) => setSenderName(e.target.value)}
+                        />
+                        <input
+                          type='email'
+                          placeholder='Email'
+                          value={email}
+                          className='block w-full rounded-md border-0 px-3 py-1.5 text-sm text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-900 sm:leading-6 md:w-10/12'
+                          onChange={(e) => setEmail(e.target.value)}
+                          required
+                        />
+                        <Button
+                          type='submit'
+                          name='Send flowers button'
+                          className='mt-4 self-center bg-purple-900 text-sm text-white'
+                        >
+                          Send Flowers
+                        </Button>
+                      </form>
+                    </div>
+                  </>
+                )}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
   );
 }
