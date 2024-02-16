8900677b40d8994d218606562d54156c716916f9
agacia1805 <agacia1805@gmail.com>
2024-02-16 16:42:26 +0000

    (feat) change email sender for sendgrid

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -23,7 +23,7 @@ export default async function handler(
 
   const message = {
     to: email,
-    from: 'pineheadcoder@gmail.com',
+    from: 'flower_sender@proton.me',
     subject: `${senderName || 'Someone'} is sending you flowers`,
     html:
       `
